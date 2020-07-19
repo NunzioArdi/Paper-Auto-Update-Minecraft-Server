2590349f5d680823bb841cd66078a56652fb94cd
Enter_name__ <nunzioconte54@gmail.com>
2020-07-19 23:07:18 +0200

    Update Suppression du logging ajout de paramètre de lancement ajout la possibilité de choisir ça version ajout possibilité de chosir la quantité de mémoir

--- script.sh
+++ script.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-#Le script est a executer à l'emplacement du .jar
-#variable à adapter
-minVer="1.14.4"
-screenName="minecraftVanillaServer"
-dir=$(pwd)
-date=$(date +"%d-%m-%Y_%H_%M_%S")
+#Fonction
+showHelp() {
+cat <<EOF
+./script.sh [OPTIONS...]
+Help:
+   -h, --help		display this help and exit.
+   -i, --info		display info and exit.
+Options:
+   --version=STRING	indique la version de minecraft à lancer ["1.16.1"]
+   --name=STRING   	donne un nom au terminal screen ["minecraftServer"]
+   --ram=STRING    	indique la ram max que java utilisera ["1G"]
+EOF
+exit 0
+}
+
+showInfo(){
+cat <<EOF
+Lance/met à jour un serveur paper sur le dernier build
+Script version: $version
+
+EOF
+exit 0;
+}
+
+app_is_installed(){ 
+	if dpkg -s "$1" &>/dev/null ; then 
+		return 0
+	else 
+		echo "$1 n\'est pas installé"
+		exit 1
+	fi
+}
+
 #source:https://code-examples.net/en/q/bb009c
+#test si il existe déjà un screen ouvert
 function find_screen {
     if screen -ls "$1" | grep -o "^\s*[0-9]*\.$1[ "$'\t'"](" --color=NEVER -m 1 | grep -oh "[0-9]*\.$1" --color=NEVER -m 1 -q >/dev/null; then
         screen -ls "$1" | grep -o "^\s*[0-9]*\.$1[ "$'\t'"](" --color=NEVER -m 1 | grep -oh "[0-9]*\.$1" --color=NEVER -m 1 2>/dev/null
@@ -15,36 +43,76 @@ function find_screen {
         return 1
     fi
 }
+################################################################################
+#Variable
+version="1.16.1"
+screenName="minecraftServer"
+dir=$(dirname "$0")
+date=$(date +"%d-%m-%Y_%H_%M_%S")
+javaXm="1G"
+version=1.0
 
+################################################################################
+#arg
+for opt in $@; do
+   optX=$opt
+   optval="${opt#*=}"
+   case "$opt" in
+      --help|-h) showHelp
+      ;;
+      --info|-i) showInfo
+      ;;
+	  --name=*) screenName=$optval
+	  ;;
+	  --ram=*) javaXm=$optval
+	  ;;
+	  --version=*) version=$optval
+	  ;;
+      *)
+         echo "Unknown option $1"
+		 exit 1
+         ;;
+  esac
+done
 
-cd /tmp
-rm latest 2>/dev/null
-if ! wget -nv https://papermc.io/api/v1/paper/${minVer}/latest &>>${dir}/logs/update-${date}.log
+################################################################################
+#Vérification des programmes installé
+app_is_installed screen
+app_is_installed wget
+if ! java -version &>/dev/null; then echo "java n\'est pas installé"; fi
+
+################################################################################
+#Récupère le numéro de la dernière version du build
+if ! wget -nv https://papermc.io/api/v1/paper/${version}/latest
 then
-        echo "Error downloading JSON from ${minVer}"
+        echo "Error downloading JSON from ${version}"
         exit 1
 fi
-
-VERSIONSTRING=$(grep -Po '"build": *\K"[^"]*"' latest)
-VERSION=$(perl -pe 's/\"//g' <<< "$VERSIONSTRING")
-fileName=("paper-${VERSION}.jar")
+BUILDSTRING=$(grep -Po '"build": *\K"[^"]*"' latest)
+BUILD=$(perl -pe 's/\"//g' <<< "$BUILDSTRING")
+fileName=("paper-${version}-${BUILD}.jar")
+rm latest 2>/dev/null
 
 cd $dir
 
+#Si le fichier n'existe pas
 if ! test -f $fileName
 then
-        oldFile=$(ls | grep "paper-[0-9]*.jar")
 
-        if ! wget -nv https://papermc.io/api/v1/paper/${minVer}/${VERSION}/download &>>${dir}/logs/update-${date}.log
+		# Récupère le nom des anciens fichier
+        oldFile=$(ls | grep "paper-$version-[0-9]*.jar")
+
+		# Télécharge le build récent
+        if ! wget -nv https://papermc.io/api/v1/paper/${version}/${BUILD}/download
         then
-                echo "Error downloading build ${VERSION}"
+                echo "Error downloading build ${BUILD}"
                 exit 2
         fi
 
         mv download $fileName
 
         #if server is on, stop
-        if find_screen $screenName &>>${dir}/logs/update-${date}.log ; then
+        if find_screen $screenName ; then
                 screen -D -R $screenName -X stuff "say Update Server, Shutdown in 60 seconds $(printf '\r')"
                 sleep 30s
 		screen -D -R $screenName -X stuff "say Shutdown in 30 seconds $(printf '\r')"
@@ -56,7 +124,8 @@ then
 		screen -D -R $screenName -X stuff "stop $(printf '\r')"
         fi
 
+		# Supprime les anciennes versions seulement après que le serveur soit stop
         rm -f $oldFile
 fi
 
-screen -d -S $screenName -m ./start.sh
+screen -d -S $screenName -m java -Xms$javaXm -Xmx$javaXm -jar $fileName
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-usage="Usage $0 <fileServer.jar>"
-
-paperFile=$(find . -name 'paper-[0-9]*.jar' | sort -nr | head -1)
-
-java -Xms128M -Xmx1536M -jar $paperFile
-
